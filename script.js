34c3ac4c3471251c6feedbb530be0875c49b11da
dnz <danut.ivanescu@outlook.com>
2021-07-24 16:01:09 +0300

    first upload for CV_24.07.2021

---
+++ script.js
@@ -0,0 +1,41 @@
+function f1() {
+    $('.educatieFormare').css('display','none');
+    $('.informatii-personale').css('display','initial');
+    $('.experientaProfesionala').css('display','none');
+    $('.competenteDigitale').css('display','none');
+    $('.contact').css('display','none');
+    }
+
+function f2() {
+    $('.educatieFormare').css('display','none');
+    $('.informatii-personale').css('display','none');
+    $('.experientaProfesionala').css('display','initial');
+    $('.competenteDigitale').css('display','none');
+    $('.contact').css('display','none');
+    }
+
+function f3() {
+    $('.educatieFormare').css('display','none');
+    $('.informatii-personale').css('display','none');
+    $('.experientaProfesionala').css('display','none');
+    $('.competenteDigitale').css('display','initial');
+    $('.contact').css('display','none');
+    }
+
+function f4() {
+    $('.educatieFormare').css('display','initial');
+    $('.informatii-personale').css('display','none');
+    $('.experientaProfesionala').css('display','none');
+    $('.competenteDigitale').css('display','none');
+    $('.contact').css('display','none');
+    }
+
+function f5() {
+    $('.educatieFormare').css('display','none');
+    $('.informatii-personale').css('display','none');
+    $('.experientaProfesionala').css('display','none');
+    $('.competenteDigitale').css('display','none');
+    $('.contact').css('display','initial');
+
+
+    }
